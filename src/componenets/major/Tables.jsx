41313e640102f795b3f4fa4349319758c18aa289
YogeshSen <YogeshSen@BIMCAP_INDIA_YS>
2024-10-24 14:18:12 +0530

    software listing done

--- src/componenets/major/Tables.jsx
+++ src/componenets/major/Tables.jsx
@@ -3,9 +3,10 @@ import BasicChips from "../minor/BasicChips"
 import Heading from "../minor/Heading"
 import UserAvatar from "../minor/UserAvatar"
 import Options from "./Options"
+import useSoftware from "../../hooks/useSoftware"
 
 const Tables = () => {
-
+    const {software} = useSoftware()
   return (
     <>
 
@@ -15,25 +16,26 @@ const Tables = () => {
                 <th>Software</th>
                 <th>Category</th>
                 <th>Version</th>
-                <th>Status</th>
                 <th>Action</th>
             </tr>
         </thead>
-        <tr>
+        {
+            software && software.length>0 && software.map((item)=>(
+                <tr key={item._id}>
             <td>
                 <Link to="/" className="d-flex d-flex-align-center gap-10">
-                    <UserAvatar /> <Heading heading="Autocad" className="fs-16"></Heading>
+                    <UserAvatar /> <Heading heading={item.name} className="fs-16"></Heading>
                 </Link>
             </td>
-            <td>Autodesk</td>
-            <td>2022</td>
-            <td>
-                <BasicChips label="Active"></BasicChips>
-            </td>
+            <td>{item.category.name}</td>
+            <td>{item.version && item.version.length>0 && item.versions.map((version)=>(version.name))}</td>
             <td>
                 <Options />
             </td>
         </tr>
+            ))
+        }
+        
     </table>
     </>
   )
